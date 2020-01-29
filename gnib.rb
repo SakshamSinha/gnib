7a4a5ea612bdfacde7cb766c3bfed4fc37d7f52c
Saksham Sinha <saksham.sinha@intel.com>
2020-01-29 13:06:08 +0000

    changed to work with the new website

--- gnib.rb
+++ gnib.rb
@@ -1,14 +1,19 @@
 require 'net/http'
 require 'openssl'
 require 'json'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'resolv-replace'
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 # uri = URI('https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=Renewal&_=1530172958153')
-uri = URI('https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?readform&cat=Work&sbcat=All&typ=New&k=11C1B124AC6ACF2E532080B68CD541E2&p=F8EDF3621F5802B8BC3CBF7226875075&_=1549443454726')
+uri = URI.parse('https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?readform&cat=All&sbcat=All&typ=Renewal&k=AA3C24A27E37C2E1574D3BD43005A70D&p=8B32615A244E4A9A81155957A290D61B')
 while true
-	res = Net::HTTP.get_response(uri)
+	#res = Net::HTTP.get_response(uri)
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	res = http.get(uri.request_uri)
 	print "\a" if JSON.parse(res.body)["empty"] != "TRUE"
 	puts res.body
 	current_time = Time.new
 	puts "At Time : " + current_time.inspect
-	sleep 2.5
+	sleep 4
 end
